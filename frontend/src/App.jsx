16efa1e3aca371ea817c2b91b11315f18a90a5a7
Mikeike34 <michaeljcantone@gmail.com>
2025-03-26 15:15:22 -0700

    installed necessary packages for chakra-ui, react, and vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button, HStack } from "@chakra-ui/react"
+
+function App() {
+
+  return (
+    <HStack>
+      <Button>Click Me</Button>
+      <Button>Click Me</Button>
+    </HStack>
+  )
+}
+
+export default App
