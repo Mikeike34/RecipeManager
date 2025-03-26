178c9602ea9dcea20a3684fc5b88c046058fb4b5
Mikeike34 <michaeljcantone@gmail.com>
2025-03-26 15:28:27 -0700

    created files for the necessary pages used in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
-import { Button, HStack } from "@chakra-ui/react"
+import { Box, Button, HStack } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
 
 function App() {
 
   return (
-    <HStack>
-      <Button>Click Me</Button>
-      <Button>Click Me</Button>
-    </HStack>
-  )
+    <Box minH ={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path ='/' element ={<LoginPage />} />
+        <Route path ='/create' element ={<CreatePage />} />
+        <Route path ='/home' element ={<HomePage />} />
+        <Route path ='/register' element ={<RegisterPage />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App
